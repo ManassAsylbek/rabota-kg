c8fd4fdbc4c6a3261a2833810913a42cbda0d95e
NurdinovE <nurdinovvv06@gmail.com>
2023-06-11 14:09:26 +0600

    header css fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,26 +3,28 @@ import HeaderLogo from '../../assets/icons/headerlogo.svg';
 import './Header.scss';
 import './Header-media.scss';
 import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
+import {useDebounce} from "../../hooks/debounce";
 
 const Header = () => {
     const [searchParams,setSearchParams] = useSearchParams();
     const [value, setValue] = useState(searchParams.get('search') || "")
     let location = useLocation()
+    const debounced = useDebounce(value)
 
     const region = searchParams.get('region') || ""
     const category = searchParams.get('category') || ""
-  /*  const debounced = useDebounce(value)*/
+
     const navigate = useNavigate()
 
-    const onChangeInputValue = (value:string) =>{
+    const onChangeInputValue = (debounced:string) =>{
         if (location.pathname === "/vacancies") {
             setSearchParams({
-                search:value,
+                search:debounced,
                 region:region,
                 category:category,
             })
         }
-        setValue(value)
+        setValue(debounced)
     }
     const handleSubmit = () => {
         let params = searchParams.toString()
@@ -55,12 +57,12 @@ const Header = () => {
             </div>
             <div className="header__main">
                 <div className="header__search">
-                    <input type="search" placeholder={'Введите вакансию, город или отрасль'} value={value} onChange={(e) => onChangeInputValue(e.target.value)}/>
+                    <input type="search" placeholder={'Введите вакансию, город или отрасль'} value={value} onChange={(e) => setValue(e.target.value)}/>
                     <button className="header__search__btn" onClick={handleSubmit}>🔍︎</button>
                 </div>
-                <button className='header__button'>
-                    <span>Подать рекламу</span>
-                </button>
+                <a className='header__button' href="https://wa.me/996709180945">
+                    Подать рекламу
+                </a>
             </div>
         </header>
     )
